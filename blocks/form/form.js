82344d1c3b40547e9cd3f682746abb5b64f4f221
Farkhad012 <pharrellmusicpro@gmail.com>
2025-01-31 16:22:16 +0300

    feat: edit info from edit modal window

---
+++ blocks/form/form.js
@@ -0,0 +1,4 @@
+export const userNameInput = document.querySelector('#name-input');
+export const occupationInput = document.querySelector('#occupation-input');
+export const formEditButton = document.querySelector('#form__edit-button');
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
-import { editButton, addButton } from '../blocks/profile/profile.js';
+import { username, occupation, editButton, addButton } from '../blocks/profile/profile.js';
 import { modalEdit, modalAdd } from '../blocks/modal/modal.js';
+import { userNameInput, occupationInput, formEditButton } from '../blocks/form/form.js';
 
 function disableScroll() {
   document.body.style.overflow = 'hidden';
@@ -7,10 +8,22 @@ function disableScroll() {
 
 editButton.addEventListener('click', () => {
   modalEdit.classList.add('opened');
-  disableScroll()
+  occupationInput.value = occupation.textContent;
+  userNameInput.value = username.textContent;
+
+  disableScroll();
 })
 
 addButton.addEventListener('click', () => {
   modalAdd.classList.add('opened')
-  disableScroll()
+
+  disableScroll();
 })
+
+formEditButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  occupation.textContent = occupationInput.value;
+  username.textContent = userNameInput.value;
+  modalEdit.classList.remove('opened');
+})
+
