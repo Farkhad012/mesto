6b1128131b6bf01b5c3d75ac5151682858724c63
Farkhad012 <pharrellmusicpro@gmail.com>
2025-02-11 17:11:07 +0300

    feat: add picture uploading

--- blocks/profile/profile.js
+++ blocks/profile/profile.js
@@ -1,4 +1,4 @@
-export const editButton = document.querySelector('button.profile__edit-button');
+export const editButton = document.querySelector('.profile__edit-button');
 export const addButton = document.querySelector('button.profile__add-button');
 export const username = document.querySelector('#username');
 export const occupation = document.querySelector('#occupation');
