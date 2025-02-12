707d58014cd6c8384d78192800899054a9985e3f
Farkhad012 <pharrellmusicpro@gmail.com>
2025-02-12 12:03:15 +0300

    refactor modal & forms

--- blocks/profile/profile.js
+++ blocks/profile/profile.js
@@ -1,4 +1,20 @@
-export const editButton = document.querySelector('.profile__edit-button');
+import { showAddForm, showEditForm } from "../form/form.js";
+import { openModal, modalTitle } from "../modal/modal.js";
+
+export const editButton = document.querySelector('button.profile__edit-button');
 export const addButton = document.querySelector('button.profile__add-button');
 export const username = document.querySelector('#username');
 export const occupation = document.querySelector('#occupation');
+
+editButton.addEventListener('click', () => {
+  openModal();
+  showEditForm();  
+  modalTitle.textContent = 'Редактировать';
+})
+
+addButton.addEventListener('click', () => {
+  openModal();
+  showAddForm();
+  modalTitle.textContent = 'Новое место';
+})
+
